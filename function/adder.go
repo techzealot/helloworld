d6b6517efffd7fb05f2bb6ab429846683227fd83
techzealot <891568426@qq.com>
2020-05-12 02:03:39 +0800

    :bento: Adding go map :egg: Adding functional programing

---
+++ function/adder.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func adder() func(int) int {
+	sum := 0
+	return func(v int) int {
+		fmt.Println("sum=", sum)
+		sum += v
+		return sum
+	}
+}
+
+func main() {
+	a := adder()
+	var s func(int) int
+	for i := 0; i < 10; i++ {
+		if i == 1 {
+			s = a
+			fmt.Println(a(i))
+		} else {
+			fmt.Println(a(i))
+		}
+
+	}
+}
---
+++ maps.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func deleteMap(m map[string]string) {
+	delete(m, "name")
+}
+
+func main() {
+	//1
+	m := map[string]string{
+		"name": "tom",
+		"age":  "20",
+	}
+	fmt.Println(m) //map[age:20 name:tom]
+
+	//2 或m2 := make(map[string]int,cap)
+	m2 := make(map[string]int)
+	fmt.Println(m2) //map[]
+
+	m2["a"] = 1
+	fmt.Println(m2)
+	//3
+	var m3 map[string]int
+	fmt.Println(m3) //map[]
+
+	//** this will panic
+	m3["a"] = 1
+
+	//** this will not panic now
+	delete(m3, "test")
+
+	for k := range m {
+		fmt.Println(k)
+	}
+	s := "test"
+	if v, ok := m[s]; ok {
+		fmt.Println(v, ok)
+	} else {
+		fmt.Println(v, ok)
+	}
+	deleteMap(m)
+	fmt.Println("after delete:", m)
+}
